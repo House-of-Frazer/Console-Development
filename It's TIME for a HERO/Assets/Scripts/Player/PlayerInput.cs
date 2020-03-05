611480a0786b141d908352e57a195cee827830a8
Joshua Conroy-Howes <44055993+OGFameless@users.noreply.github.com>
2020-03-05 16:35:50 +0000

    Created PlayerInput script and Collectable Script
    added input script and edited the inputs to be taken from this script.
    added collectable script that has an int for the coins that the player picks up.

---
+++ It's TIME for a HERO/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    PlayerMovement movement; //Reference for the PlayerMovement script;
+
+    private void Awake()
+    {
+        movement = GetComponent<PlayerMovement>(); //Setup reference for the PlayerMovement script;
+    }
+    void BasicMovement() //Controls the inputs for the player's basic movment
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement.Forward();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement.Left();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement.Right();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement.Backward();
+        }
+    }
+
+    void ExtraMovement() //Controls the inputs for the player's advanced movement
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            movement.RotateLeft();
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            movement.RotateRight();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && movement.PlayerGrounded)
+        {
+            StartCoroutine(movement.Jumping());
+        }
+    }
+
+    void TimeTravel() //Controls the inputs for the player's time travel mechanic
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            movement.TimeTravel();
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BasicMovement();
+        ExtraMovement();
+        TimeTravel();
+    }
+}
--- It's TIME for a HERO/Assets/Scripts/Player/PlayerMovement.cs
+++ It's TIME for a HERO/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody RB;
-    bool PlayerGrounded;
+    public bool PlayerGrounded;
 
     public int moveSpeed;
 
@@ -23,67 +23,50 @@ public class PlayerMovement : MonoBehaviour
         levelPresent.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Forward() //Controls for movement using W when the player presses and holds the key
     {
-        //requires the player to press space to jump
-        if (Input.GetKeyDown(KeyCode.Space) && PlayerGrounded)
-        {
-            StartCoroutine(Jumping());
-        }
-
-        //test mesh swap
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            if (inPast == false)
-            {
-                levelPast.SetActive(true);
-                levelPresent.SetActive(false);
-                inPast = true;
-            }
-            else
-            {
-                levelPast.SetActive(false);
-                levelPresent.SetActive(true);
-                inPast = false;
-            }
-        }
-
-        //Controles for movement using WASD when the player presses and holds the key
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate((transform.forward * moveSpeed * Time.deltaTime), Space.World);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime, Camera.main.transform);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate((-transform.forward * moveSpeed * Time.deltaTime), Space.World);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime, Camera.main.transform);
-        }
-
-        //Controls for movement of the players rotation using Q and E keys to look around and behind themselves
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(Vector3.up * 2);
-        }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(-Vector3.up * 2);
-        }
-
+        transform.Translate((transform.forward * moveSpeed * Time.deltaTime), Space.World);
+    }
+    public void Left() //Controls for movement using A when the player presses and holds the key
+    {
+        transform.Translate(Vector3.left * Time.deltaTime, Camera.main.transform);
+    }
+    public void Right() //Controls for movement using S when the player presses and holds the key
+    {
+        transform.Translate(Vector3.right * Time.deltaTime, Camera.main.transform);
+    }
+    public void Backward() //Controls for movement using D when the player presses and holds the key
+    {
+        transform.Translate((-transform.forward * Time.deltaTime * Time.deltaTime), Space.World);
     }
 
-    IEnumerator Jumping()
+    public void RotateLeft()  //Controls for movement of the players rotation using Q
+    {
+        transform.Rotate(-Vector3.up * 2);
+    }
+
+    public void RotateRight() //Controls for movement of the players rotation using E
+    {
+        transform.Rotate(Vector3.up * 2);   
+    }
+
+    public void TimeTravel() //test mesh swap
+    {
+        if (inPast == false)
+        {
+            levelPast.SetActive(true);
+            levelPresent.SetActive(false);
+            inPast = true;
+        }
+        else
+        {
+            levelPast.SetActive(false);
+            levelPresent.SetActive(true);
+            inPast = false;
+        }
+    }
+
+    public IEnumerator Jumping()
     {
         RB.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
         PlayerGrounded = false;
