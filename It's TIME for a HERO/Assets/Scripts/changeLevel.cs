440460dcb2956181b588b93498612995493d170f
SlimmestCognito <rhys-e-davis@virginmedia.com>
2020-03-10 15:40:13 +0000

    Making portal
    - Created temp portal object
    - Created portal script to change level
    - Added scenes to project build settings

---
+++ It's TIME for a HERO/Assets/Scripts/changeLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class changeLevel : MonoBehaviour
+{
+    public string LevelName;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            changeToLevel(LevelName);
+        }
+    }
+
+    public static void changeToLevel(string lvl) {
+        SceneManager.LoadScene(lvl);
+
+    }
+}
