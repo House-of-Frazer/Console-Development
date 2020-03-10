b852908417c3487c4342e4b1f775099630660d43
Frazer <F.Green144@gmail.com>
2020-03-10 16:06:04 +0000

    Level

---
+++ Time To Be A Hero (Frazer)/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject topDownCamera;
+
+    public bool mainCamActive;
+
+    void Start()
+    {
+        mainCamera.SetActive(true);
+        topDownCamera.SetActive(false);
+        mainCamActive = true;
+    }
+
+   void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //if (mainCamActive == true)
+            //{
+                mainCamera.SetActive(false);
+                topDownCamera.SetActive(true);
+                mainCamActive = false;
+            //}
+            /*if (mainCamActive == false)
+            {
+                mainCamera.SetActive(true);
+                topDownCamera.SetActive(false);
+                mainCamActive = true;
+            }*/
+       
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            mainCamera.SetActive(true);
+            topDownCamera.SetActive(false);
+            mainCamActive = true;
+        }
+    }
+}
---
+++ Time To Be A Hero (Frazer)/Assets/Scripts/MakeOBJ.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class MakeOBJ : MonoBehaviour
+{
+    
+    Ray ray;
+    RaycastHit hit;
+    public GameObject prefab;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        GraphicRaycaster gr = this.GetComponent<GraphicRaycaster>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //ray
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Creates an object from where you clicked mutilpies the x coordinates by 12, and divides the z coordinates by 2-700, to fit them on larger map
+                GameObject obj = Instantiate(prefab, new Vector3(hit.point.x * 12, hit.point.y, hit.point.z/2-700), Quaternion.identity) as GameObject;
+                obj.transform.localScale += new Vector3(2241, 1368, 2165);
+            }
+
+        }
+    }
+    
+}
