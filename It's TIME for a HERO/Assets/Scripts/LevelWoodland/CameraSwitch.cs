4ea97e92c9bb53edc017e537f442695a05105775
MrSlimCognito <rhys-e-davis@virginmedia.com>
2020-03-11 14:23:55 +0000

    Frazer level impementation
    - Added woodland level to master
    - Added wodland scripts and assets
    - Added portal to get from woodland to hub
    - Added hub decoration to woodland island
    - Added hub decoration to IceMountain island

---
+++ It's TIME for a HERO/Assets/Scripts/LevelWoodland/CameraSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject topDownCamera;
+
+    public static bool mainCamActive;
+    //main camera is the player camera
+
+    void Start()
+    {
+        mainCamera.SetActive(true);
+        topDownCamera.SetActive(false);
+        mainCamActive = true;
+    }
+
+   void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //if (mainCamActive == true)
+            //{
+                mainCamera.SetActive(false);
+                topDownCamera.SetActive(true);
+                mainCamActive = false;
+            //}
+            /*if (mainCamActive == false)
+            {
+                mainCamera.SetActive(true);
+                topDownCamera.SetActive(false);
+                mainCamActive = true;
+            }*/
+       
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            mainCamera.SetActive(true);
+            topDownCamera.SetActive(false);
+            mainCamActive = true;
+        }
+    }
+}
---
+++ It's TIME for a HERO/Assets/Scripts/LevelWoodland/MakeOBJ.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class MakeOBJ : MonoBehaviour
+{
+    
+    Ray ray;
+    RaycastHit hit;
+    public GameObject prefab;
+       
+
+    // Use this for initialization
+    void Start()
+    {
+        GraphicRaycaster gr = this.GetComponent<GraphicRaycaster>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //ray
+        if (CameraSwitch.mainCamActive == false) {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //Creates an object from where you clicked mutilpies the x coordinates by 12, and divides the z coordinates by 2-700, to fit them on larger map
+                    GameObject obj = Instantiate(prefab, new Vector3(hit.point.x, hit.point.y - 1066, hit.point.z), Quaternion.identity) as GameObject;
+                    obj.transform.localScale = new Vector3(40, 40, 40);
+                }
+
+            }
+        }
+    }
+    
+}
