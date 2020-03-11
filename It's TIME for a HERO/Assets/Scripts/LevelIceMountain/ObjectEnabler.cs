bb51290219b0b19fb0645b23a2470dc20884b6b5
MrSlimCognito <rhys-e-davis@virginmedia.com>
2020-03-11 16:50:45 +0000

    IceMountain Puzzle working & shaders
    - Ice mountain now has puzzle
    - Added shader scripts, but forgot textures
    - next update, add shader textures to get them working

--- It's TIME for a HERO/Assets/Scripts/LevelIceMountain/ObjectEnabler.cs
+++ It's TIME for a HERO/Assets/Scripts/LevelIceMountain/ObjectEnabler.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ObjectEnabler : MonoBehaviour
 {
+    bool _enabled;
+    public GameObject obj1, obj2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _enabled = false;
+        obj1.SetActive(false);
+        obj2.SetActive(false);
+               
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_enabled)
+        {
+            obj1.SetActive(true);
+            obj2.SetActive(true);
+           
+        }
+     }
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.gameObject.tag == "Player") {
+            _enabled = true;
+        }
     }
 }
