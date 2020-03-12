cb56838d395a1c01733b259ee1407cc573b4132a
SlimmestCognito <rhys-e-davis@virginmedia.com>
2020-03-12 09:30:26 +0000

    Update input manager
    - Added extra stuff to input manager to stop it from yelling

--- It's TIME for a HERO/Assets/Scripts/EnemyHealthBar/PlayerMovementN.cs
+++ It's TIME for a HERO/Assets/Scripts/EnemyHealthBar/PlayerMovementN.cs
@@ -11,6 +11,8 @@ public class PlayerMovementN : MonoBehaviour
 
     private Vector3 moveDirection;
 
+    float axisHorizontal, axisVertical;
+
 
     // Start is called before the first frame update
     void Start()
@@ -59,6 +61,7 @@ public class PlayerMovementN : MonoBehaviour
             transform.Rotate(-Vector3.up * 2);
         }
 
+        */
 
         //moveDirection = (transform.forward * Input.GetAxis("MoveVertical") + transform.right * Input.GetAxis("MoveHorizontal"));
         //**moveDirection = (transform.right * Input.GetAxis("MoveHorizontal"));
@@ -70,25 +73,29 @@ public class PlayerMovementN : MonoBehaviour
 
         //**moveDirection = (transform.forward * Input.GetAxis("MoveHorizontal") * movement);
 
-    */
+        axisHorizontal = Input.GetAxis("Horizontal");
+        axisVertical = Input.GetAxis("Vertical");
+
+
+    
     }
 
     public void Forward() //Controls for movement using W when the player presses and holds the key
     {
-        transform.Translate(Vector3.forward * movement, Camera.main.transform);
+        transform.Translate(Vector3.forward * axisVertical, Camera.main.transform);
     }
 
     public void Left() //Controls for movement using A when the player presses and holds the key
     {
-        transform.Translate(Vector3.left * movement, Camera.main.transform);
+        transform.Translate(Vector3.left * axisHorizontal, Camera.main.transform);
     }
     public void Right() //Controls for movement using S when the player presses and holds the key
     {
-        transform.Translate(Vector3.right * movement, Camera.main.transform);
+        transform.Translate(Vector3.right * axisHorizontal, Camera.main.transform);
     }
     public void Backward() //Controls for movement using D when the player presses and holds the key
     {
-        transform.Translate(-Vector3.forward * movement, Camera.main.transform);
+        transform.Translate(-Vector3.forward * axisVertical, Camera.main.transform);
     }
 
     public void RotateLeft()  //Controls for movement of the players rotation using Q
