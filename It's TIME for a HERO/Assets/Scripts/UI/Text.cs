1b17dd808bfff5cae50d3f3a14795f9ce7d24e81
faizananwar1738 <56159002+faizananwar1738@users.noreply.github.com>
2020-03-12 03:22:37 +0000

    Faiz's stuff

---
+++ It's TIME for a HERO/Assets/Scripts/UI/Text.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+
+{
+
+    public TextMesh text1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        text1.text = "Press 'T' to Talk";
+    }
+
+    public static implicit operator Text(bool v)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ It's TIME for a HERO/Assets/Scripts/camera/CameraMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    //public Transform Player;
+    public Camera FirstPersonCam, ThirdPersonCam;
+    public KeyCode TKey;
+    public bool camSwitch = false;
+    public GameObject shoptext;
+
+    void Start()
+    {
+        shoptext.SetActive(false);
+}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(TKey))
+        {
+            camSwitch = !camSwitch;
+            FirstPersonCam.gameObject.SetActive(camSwitch);
+            ThirdPersonCam.gameObject.SetActive(!camSwitch);
+            shoptext.gameObject.SetActive(true);
+        }
+    }
+}
