e7da497f68e0c4d75bc0b86d09cfa782bc3e5d6a
faizananwar1738 <56159002+faizananwar1738@users.noreply.github.com>
2020-05-19 04:44:27 +0100

    upload
    minus
    -buy stuff
    -anim (done l8er)

---
+++ It's TIME for a HERO/Assets/Scripts/coinCollection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinCollection : MonoBehaviour
+{
+
+        public AudioSource moneySound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //play coin noise
+            moneySound.Play();
+            //add 1 to money every time gain coin
+            MoneySystem.theMoney += 1;  
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ It's TIME for a HERO/Assets/Scripts/shop1.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityStandardAssets.Character.Player;
+
+public class shop1 : MonoBehaviour
+{
+    public GameObject shopInventory;
+    public GameObject Item1Text;
+    public GameObject Item2Text;
+    public GameObject Item3Text;
+    public GameObject Item4Text;
+    public GameObject itemCompletion;
+    public GameObject completeText;
+   
+
+    public GameObject shopPanel;
+    public KeyCode TKey;
+
+    //public GameObject thePlayer;
+   
+
+    void Start()
+    {
+        //shopInventory.SetActive(false);
+        itemCompletion.SetActive(false);
+        // completeText.SetActive(false);
+
+    }
+
+    void OnTriggerEnter()
+    {
+            shopInventory.SetActive(true);
+            shopStuff.shopNumber = 1;
+
+            Item1Text.GetComponent<Text>().text = "" + shopStuff.Item1;
+            Item2Text.GetComponent<Text>().text = "" + shopStuff.Item2;
+            Item3Text.GetComponent<Text>().text = "" + shopStuff.Item3;
+            Item4Text.GetComponent<Text>().text = "" + shopStuff.Item4;
+    }
+
+    public void Item1()
+    {
+        itemCompletion.SetActive(true);
+        completeText.GetComponent<Text>().text = "Are you sure you wish to buy " + shopStuff.Item1 + "?";
+    }
+    public void Item2()
+    {
+        itemCompletion.SetActive(true);
+        completeText.GetComponent<Text>().text = "Are you sure you wish to buy " + shopStuff.Item2 + "?";
+    }
+    public void Item3()
+    {
+        itemCompletion.SetActive(true);
+        completeText.GetComponent<Text>().text = "Are you sure you wish to buy " + shopStuff.Item3 + "?";
+    }
+    public void Item4()
+    {
+        itemCompletion.SetActive(true);
+        completeText.GetComponent<Text>().text = "Are you sure you wish to buy " + shopStuff.Item4 + "?";
+    }
+
+    public void cancelTransaction()
+    {
+        itemCompletion.SetActive(false);
+    }
+}
